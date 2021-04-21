b91b7917977a68ccf9b67daa9035414f31898701
brajendra kumar <brajendrak0068@gmail.com>
2021-04-22 05:02:10 +0530

    Added RTCVideoView

---
+++ ios/RCTWebRTC/RTCVideoView.h
@@ -0,0 +1,65 @@
+#import <Foundation/Foundation.h>
+#import <objc/runtime.h>
+
+#import <React/RCTLog.h>
+#if !TARGET_OS_OSX
+#import <WebRTC/RTCEAGLVideoView.h>
+#endif
+#import <WebRTC/RTCMediaStream.h>
+#if !TARGET_OS_OSX
+#import <WebRTC/RTCMTLVideoView.h>
+#else
+#import <WebRTC/RTCMTLNSVideoView.h>
+#endif
+#import <WebRTC/RTCVideoTrack.h>
+
+#import "RTCVideoViewManager.h"
+#import "WebRTCModule.h"
+#import "RTCVideoView.h"
+#import "Enum.h"
+
+#if !TARGET_OS_OSX
+@interface RTCVideoView : UIView <RTCVideoViewDelegate>
+#else
+@interface RTCVideoView : NSView <RTCVideoViewDelegate>
+#endif
+
+/**
+ * The indicator which determines whether this {@code RTCVideoView} is to mirror
+ * the video specified by {@link #videoTrack} during its rendering. Typically,
+ * applications choose to mirror the front/user-facing camera.
+ */
+@property (nonatomic) BOOL mirror;
+
+/**
+ * In the fashion of
+ * https://www.w3.org/TR/html5/embedded-content-0.html#dom-video-videowidth
+ * and https://www.w3.org/TR/html5/rendering.html#video-object-fit, resembles
+ * the CSS style {@code object-fit}.
+ */
+@property (nonatomic) RTCVideoViewObjectFit objectFit;
+
+/**
+ * The {@link RRTCVideoRenderer} which implements the actual rendering and which
+ * fits within this view so that the rendered video preserves the aspect ratio of
+ * {@link #_videoSize}.
+ */
+#if !TARGET_OS_OSX
+@property (nonatomic, readonly) __kindof UIView<RTCVideoRenderer> *videoView;
+#else
+@property (nonatomic, readonly) __kindof NSView<RTCVideoRenderer> *videoView;
+#endif
+
+/**
+ * The {@link RTCVideoTrack}, if any, which this instance renders.
+ */
+@property (nonatomic, strong) RTCVideoTrack *videoTrack;
+
+/**
+ * Reference to the main WebRTC RN module.
+ */
+@property (nonatomic, weak) WebRTCModule *module;
+
+@end
+
+
--- ios/RCTWebRTC/RTCVideoViewManager.m
+++ ios/RCTWebRTC/RTCVideoViewManager.m
@@ -22,78 +22,9 @@
 
 #import "RTCVideoViewManager.h"
 #import "WebRTCModule.h"
+#import "RTCVideoView.h"
+#import "Enum.h"
 
-/**
- * In the fashion of
- * https://www.w3.org/TR/html5/embedded-content-0.html#dom-video-videowidth
- * and https://www.w3.org/TR/html5/rendering.html#video-object-fit, resembles
- * the CSS style {@code object-fit}.
- */
-typedef NS_ENUM(NSInteger, RTCVideoViewObjectFit) {
-  /**
-   * The contain value defined by https://www.w3.org/TR/css3-images/#object-fit:
-   *
-   * The replaced content is sized to maintain its aspect ratio while fitting
-   * within the element's content box.
-   */
-  RTCVideoViewObjectFitContain,
-  /**
-   * The cover value defined by https://www.w3.org/TR/css3-images/#object-fit:
-   *
-   * The replaced content is sized to maintain its aspect ratio while filling
-   * the element's entire content box.
-   */
-  RTCVideoViewObjectFitCover
-};
-
-/**
- * Implements an equivalent of {@code HTMLVideoElement} i.e. Web's video
- * element.
- */
-
-#if !TARGET_OS_OSX
-@interface RTCVideoView : UIView <RTCVideoViewDelegate>
-#else
-@interface RTCVideoView : NSView <RTCVideoViewDelegate>
-#endif
-
-/**
- * The indicator which determines whether this {@code RTCVideoView} is to mirror
- * the video specified by {@link #videoTrack} during its rendering. Typically,
- * applications choose to mirror the front/user-facing camera.
- */
-@property (nonatomic) BOOL mirror;
-
-/**
- * In the fashion of
- * https://www.w3.org/TR/html5/embedded-content-0.html#dom-video-videowidth
- * and https://www.w3.org/TR/html5/rendering.html#video-object-fit, resembles
- * the CSS style {@code object-fit}.
- */
-@property (nonatomic) RTCVideoViewObjectFit objectFit;
-
-/**
- * The {@link RRTCVideoRenderer} which implements the actual rendering and which
- * fits within this view so that the rendered video preserves the aspect ratio of
- * {@link #_videoSize}.
- */
-#if !TARGET_OS_OSX
-@property (nonatomic, readonly) __kindof UIView<RTCVideoRenderer> *videoView;
-#else
-@property (nonatomic, readonly) __kindof NSView<RTCVideoRenderer> *videoView;
-#endif
-
-/**
- * The {@link RTCVideoTrack}, if any, which this instance renders.
- */
-@property (nonatomic, strong) RTCVideoTrack *videoTrack;
-
-/**
- * Reference to the main WebRTC RN module.
- */
-@property (nonatomic, weak) WebRTCModule *module;
-
-@end
 
 @implementation RTCVideoView {
   /**
